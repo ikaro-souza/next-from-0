d4a8f7aed0358a8d1f42968a73e056d22d4dfb56
ikaro-souza <ikaro.lisarb@gmail.com>
2022-09-08 23:32:37 -0300

    create shopping list items page

--- src/pages/shopping-lists/[id].tsx
+++ src/pages/shopping-lists/[id].tsx
@@ -1,7 +1,39 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { trpc } from '../../lib/trpc';
+import { getServerAuthSession } from '../../server/common/get-server-auth-session';
 
 const ShoppingListDetails: NextPage = () => {
-  return <div>[id]</div>;
+  const router = useRouter();
+  const { id } = router.query;
+  const { data } = trpc.useQuery(['shopping-lists.find-one', id as string], {
+    enabled: !!id,
+  });
+
+  return (
+    <div>
+      {data?.map((d: any) => (
+        <p key={d.id}>{d.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/auth/signin?origin=${ctx.resolvedUrl}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default ShoppingListDetails;
--- src/server/router/shopping-list.ts
+++ src/server/router/shopping-list.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod';
 import { createProtectedRouter } from '../protected-router';
 import { shoppingList } from '../schemas';
 
@@ -16,6 +17,24 @@ export const shoppingListsRouter = createProtectedRouter()
       });
 
       return lists;
+    },
+  })
+  .query('find-one', {
+    input: z.string(),
+    resolve: async ({ ctx, input }) => {
+      const shoppingList = await ctx.prisma.shoppingList.findFirstOrThrow({
+        where: {
+          id: input,
+          user: {
+            id: ctx.session.user.id,
+          },
+        },
+        include: {
+          items: true,
+        },
+      });
+
+      return (shoppingList as any).items;
     },
   })
   .mutation('add', {
