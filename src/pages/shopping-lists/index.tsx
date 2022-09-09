ccd89d232c04c8ec94817ed56faf9411285337e3
ikaro-souza <ikaro.lisarb@gmail.com>
2022-09-08 22:15:07 -0300

    add shopping list with input

--- src/pages/shopping-lists/index.tsx
+++ src/pages/shopping-lists/index.tsx
@@ -22,11 +22,30 @@ const Home: NextPage = () => {
   const queryClient = trpc.useContext();
   const { data, isLoading } = trpc.useQuery(['shopping-lists.find-all']);
   const [listFilter] = useAtom(listFilterAtom);
-  const addShoppingListMutation = trpc.useMutation(['shopping-lists.add'], {
+  const [openAddShoppingListThingy, setOpenAddShoppingListThingy] =
+    React.useState(false);
+  const { mutate } = trpc.useMutation(['shopping-lists.add'], {
     onSuccess() {
       queryClient.invalidateQueries(['shopping-lists.find-all']);
     },
+    onMutate(newShoppingList) {
+      setOpenAddShoppingListThingy(false);
+      queryClient.cancelQuery(['shopping-lists.find-all']);
+      queryClient.setQueryData(['shopping-lists.find-all'], old => [
+        ...(old ?? []),
+        newShoppingList as any,
+      ]);
+      return queryClient.getQueryData(['shopping-lists.find-all']);
+    },
   });
+  const newShoppingListRef = React.useRef<HTMLInputElement | null>(null);
+
+  const onSubmitNewShoppingList = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate({
+      name: newShoppingListRef.current?.value ?? '',
+    });
+  };
 
   const list = React.useMemo(() => {
     return (
@@ -39,7 +58,7 @@ const Home: NextPage = () => {
           )
           .map(sl => (
             <Link key={sl.id} href={`shopping-lists/${sl.id}`}>
-              <li className='w-full cursor-pointer border-b border-b-outline p-2 text-center last:border-b-0 dark:border-b-outline-dark'>
+              <li className='w-full cursor-pointer border-b border-b-outline py-3 text-center last:border-b-0 dark:border-b-outline-dark'>
                 {sl.name}
               </li>
             </Link>
@@ -51,18 +70,40 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Quick shopping list ⚡ | Manage your lists</title>
+        <title>Quick shopping list ⚡| Manage your lists</title>
       </Head>
       {isLoading && <PageSpinner />}
       <main className='h-[100vh]'>
         <Navbar />
         {list}
+        {openAddShoppingListThingy && (
+          <form
+            className={classNames(
+              'flex w-full items-center px-6',
+              openAddShoppingListThingy ? 'visible' : 'invisible',
+            )}
+            onSubmit={onSubmitNewShoppingList}
+          >
+            <input
+              type='text'
+              className='h-12 flex-grow border-t border-none border-t-outline bg-transparent pl-5 text-center placeholder:text-center focus:outline-none dark:border-b-outline-dark'
+              autoFocus
+              placeholder='new shopping list'
+              maxLength={30}
+              ref={newShoppingListRef}
+            />
+            <span
+              role='button'
+              className='w-5'
+              onClick={() => setOpenAddShoppingListThingy(false)}
+            >
+              <XMarkIcon />
+            </span>
+            <input type='submit' hidden />
+          </form>
+        )}
       </main>
-      <Fab
-        onClick={() =>
-          addShoppingListMutation.mutate({ name: 'test-shopping-list' })
-        }
-      >
+      <Fab onClick={() => setOpenAddShoppingListThingy(true)}>
         <PlusCircleIcon />
       </Fab>
     </>
@@ -79,7 +120,7 @@ const Navbar: React.FC = () => {
 
   return (
     <header className='border-b border-b-outline dark:border-b-outline-dark'>
-      <div className='flex h-12 w-full items-center px-4'>
+      <div className='flex h-14 w-full items-center px-4'>
         <h1 className='flex-grow pl-6 text-center text-xl font-semibold'>
           Your lists
         </h1>
